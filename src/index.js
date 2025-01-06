a09adcfd7fd4b5d40c8f37e9f8e1f418abe6e378
dev-rodrygo <rodrigo.off29@gmail.com>
2025-01-05 23:27:34 -0300

    adicionando mais funções ...

--- src/index.js
+++ src/index.js
@@ -68,6 +68,14 @@ async function getRandomBlock() {
   return result;
 }
 
+async function logRollResult(characterName, block, diceResult, attribute) {
+  console.log(
+    `${characterName} 🎲 Rolou um de ${block} ${diceResult} + ${attribute} = ${
+      diceResult + attribute
+    }`
+  );
+}
+
 async function playRaceEngine(character1, character2) {
   for (let round = 1; round <= 5; round++) {
     console.log(`🏁Rodada ${round}`);
@@ -77,21 +85,74 @@ async function playRaceEngine(character1, character2) {
     console.log(`Bloco: ${block}`);
 
     //Rolar Dados
-    let diceResult1 = rollDice();
-    let diceResult2 = rollDice();
+    let diceResult1 = await rollDice();
+    let diceResult2 = await rollDice();
 
     //test Habilidade
-    let totalSkill1 = 0;
     let totalSkill2 = 0;
+    let totalSkill1 = 0;
 
     if (block === "RETA") {
       totalSkill1 = diceResult1 + character1.VELOCIDADE;
       totalSkill2 = diceResult2 + character2.VELOCIDADE;
+
+      await logRollResult(
+        character1.NOME,
+        "Velocidade",
+        diceResult1,
+        character1.VELOCIDADE
+      );
+      await logRollResult(
+        character2.NOME,
+        "Velocidade",
+        diceResult2,
+        character2.VELOCIDADE
+      );
     }
     if (block === "CURVA") {
+      totalSkill1 = diceResult1 + character1.MANOBRABILIDADE;
+      totalSkill2 = diceResult2 + character2.MANOBRABILIDADE;
+
+      await logRollResult(
+        character1.NOME,
+        "Manobrabilidade",
+        diceResult1,
+        character1.MANOBRABILIDADE
+      );
+      await logRollResult(
+        character2.NOME,
+        "Manobrabilidade",
+        diceResult2,
+        character2.MANOBRABILIDADE
+      );
     }
     if (block === "CONFRONTO") {
+      let powerResult1 = diceResult1 + character1.PODER;
+      let powerResult2 = diceResult2 + character2.PODER;
+
+      await logRollResult(
+        character1.NOME,
+        "Confront",
+        diceResult1,
+        character1.PODER
+      );
+      await logRollResult(
+        character1.NOME,
+        "Confronto",
+        diceResult2,
+        character2.PODER
+      );
     }
+
+    if (totalSkill1 > totalSkill2) {
+      console.log(`${character1.NOME} recebeu um ponto!`);
+      totalSkill1++;
+    } else {
+      console.log(`${character2.NOME} recebeu um ponto!`);
+      totalSkill2++;
+    }
+
+    console.log("🏁🚦-----------------------------🏁🚦\n");
   }
 }
 
