dc6dbf95c7879f4bb6f3702cac3f9e9b86665c4b
dev-rodrygo <rodrigo.off29@gmail.com>
2025-01-12 21:53:03 -0300

    entendendo arrays

--- array.js
+++ array.js
@@ -1,9 +1,21 @@
 let numbers = [];
+let numbersPar = [];
+let numbersImpar = [];
 
 function addNumbers(number) {
   for (let i = 0; i < number; i++) {
-    numbers.push(i * 2);
+    numbers.push(i);
+
+    let x = numbers[i];
+
+    if (x % 2 === 0) {
+      numbersPar.push(x);
+    } else {
+      numbersImpar.push(x);
+    }
   }
 }
-addNumbers(10);
-console.log(numbers);
+addNumbers(20);
+console.log(`\nNumeros: ${numbers}\n`);
+console.log(`Numeros pares: ${numbersPar}\n`);
+console.log(`Numeros impares: ${numbersImpar}`);
