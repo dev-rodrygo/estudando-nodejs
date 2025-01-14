76c2c29cf1abe766928c3f1081b34e2e4bf65ce1
dev-rodrygo <rodrigo.off29@gmail.com>
2025-01-14 19:47:34 -0300

    entendendo for loop

---
+++ src/for-types.js
@@ -0,0 +1,9 @@
+// for loop
+const loop = [5, 6, 7, 8, 9];
+
+//o for percorre toda a array loop
+for (let i = 0; i < loop.length; i++) {
+  const element = loop[i];
+
+  console.log(`${element} x ${element} = ${element * element}`);
+}
