d4a7922df9a580a91fb31a29c43b84700577a2b1
dev-rodrygo <rodrigo.off29@gmail.com>
2025-02-07 21:11:46 -0300

    better organizing the map folder

---
+++ fundaments/Map/clear/exemples1.js
@@ -0,0 +1,8 @@
+let usuarios = new Map()
+usuarios.set('Alice', 25)
+usuarios.set('Bob', 30)
+
+console.log(usuarios.size) // 2
+
+usuarios.clear() // Remove todas as entradas
+console.log(usuarios.size) // 0
