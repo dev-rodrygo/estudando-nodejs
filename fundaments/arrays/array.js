e3d331f40f4d11150b6e5cc55a8ee910c0ac03ea
dev-rodrygo <rodrigo.off29@gmail.com>
2025-02-17 22:38:25 -0300

    adicionando exemplo de array

--- fundaments/arrays/array.js
+++ fundaments/arrays/array.js
@@ -1,22 +1,50 @@
-const numbers = [];
-const numbersPar = [];
-const numbersImpar = [];
+const pessoas = [
+    {
+        nome: 'João Silva',
+        dataNascimento: '1990-05-15',
+        idade: 33,
+        endereco: 'Rua das Flores, 123, Centro, São Paulo - SP',
+        cpf: '123.456.789-00',
+        telefone: '(11) 98765-4321',
+        genero: 'Masculino',
+    },
+    {
+        nome: 'Maria Oliveira',
+        dataNascimento: '1985-10-22',
+        idade: 38,
+        endereco: 'Avenida Brasil, 456, Jardim América, Rio de Janeiro - RJ',
+        cpf: '987.654.321-00',
+        telefone: '(21) 99876-5432',
+        genero: 'Feminino',
+    },
+    {
+        nome: 'Carlos Souza',
+        dataNascimento: '2000-03-08',
+        idade: 23,
+        endereco: 'Rua das Palmeiras, 789, Vila Nova, Belo Horizonte - MG',
+        cpf: '456.789.123-00',
+        telefone: '(31) 98765-1234',
+        genero: 'Masculino',
+    },
+    {
+        nome: 'Ana Costa',
+        dataNascimento: '1995-07-30',
+        idade: 28,
+        endereco: 'Rua dos Coqueiros, 321, Praia Grande, Santos - SP',
+        cpf: '321.654.987-00',
+        telefone: '(13) 98765-6789',
+        genero: 'Feminino',
+    },
+    {
+        nome: 'Pedro Almeida',
+        dataNascimento: '1980-12-05',
+        idade: 43,
+        endereco: 'Avenida Paulista, 1010, Bela Vista, São Paulo - SP',
+        cpf: '654.321.987-00',
+        telefone: '(11) 99876-1234',
+        genero: 'Masculino',
+    },
+]
 
-function addNumbers(number) {
-  for (let i = 0; i < number; i++) {
-    numbers.push(i);
 
-    const x = numbers[i];
-
-    if (x % 2 === 0) {
-      numbersPar.push(x);
-    } else {
-      numbersImpar.push(x);
-    }
-  }
-}
-
-addNumbers(100);
-console.log(`\nNumeros: ${numbers}\n`);
-console.log(`Numeros pares: ${numbersPar}\n`);
-console.log(`Numeros impares: ${numbersImpar}`);
+console.log(pessoas)
